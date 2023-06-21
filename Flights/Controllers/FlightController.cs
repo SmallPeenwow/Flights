6941cf63a94ed1c96f08e1c82a00f514d168dbea
SmallPeenwow <bowdencamryn@gmail.com>
2023-06-21 08:27:54 +0200

    Read Models Created

--- Flights/Controllers/FlightController.cs
+++ Flights/Controllers/FlightController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Flights.ReadModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Flights.Controllers
 {
@@ -13,12 +14,68 @@ namespace Flights.Controllers
             _logger = logger;
         }
 
+        Random random = new Random();
+
         [HttpGet]
-        public IEnumerable<string> Search() => new string[]
+        public IEnumerable<FlightRm> Search() => new FlightRm[]
         {
-            "American Airlines",
-            "Deutsche BA",
-            "British Airways",
+           new (
+                Guid.NewGuid(), "American Airlines",
+                random.Next(90, 5000).ToString(),
+                new TimePlaceRm("Los Angeles", DateTime.Now.AddHours(random.Next(1, 3))),
+                new TimePlaceRm("Istanbul", DateTime.Now.AddHours(random.Next(4, 10))),
+                random.Next(1, 853)
+               ),
+           new (
+                Guid.NewGuid(), "Deutsche BA",
+                random.Next(90, 5000).ToString(),
+                new TimePlaceRm("Munchen", DateTime.Now.AddHours(random.Next(1, 10))),
+                new TimePlaceRm("Schiphol", DateTime.Now.AddHours(random.Next(4, 15))),
+                random.Next(1, 853)
+               ),
+           new (
+                Guid.NewGuid(), "British Airways",
+                random.Next(90, 5000).ToString(),
+                new TimePlaceRm("London, England", DateTime.Now.AddHours(random.Next(1, 15))),
+                new TimePlaceRm("Vizzola-Ticino", DateTime.Now.AddHours(random.Next(4, 18))),
+                random.Next(1, 853)
+               ),
+           new (
+                Guid.NewGuid(), "Basiq Air",
+                random.Next(90, 5000).ToString(),
+                new TimePlaceRm("Amsterdam", DateTime.Now.AddHours(random.Next(1, 21))),
+                new TimePlaceRm("Glasgow, Scotland", DateTime.Now.AddHours(random.Next(4, 21))),
+                random.Next(1, 853)
+               ),
+           new (
+                Guid.NewGuid(), "BB Heliag",
+                random.Next(90, 5000).ToString(),
+                new TimePlaceRm("Zurich", DateTime.Now.AddHours(random.Next(1, 23))),
+                new TimePlaceRm("Baku", DateTime.Now.AddHours(random.Next(4, 25))),
+                random.Next(1, 853)
+               ),
+           new (
+                Guid.NewGuid(), "Adria Airways",
+                random.Next(90, 5000).ToString(),
+                new TimePlaceRm("Ljubljana", DateTime.Now.AddHours(random.Next(1, 15))),
+                new TimePlaceRm("Warsaw", DateTime.Now.AddHours(random.Next(4, 19))),
+                random.Next(1, 853)
+               ),
+           new (
+                Guid.NewGuid(), "ABA Air",
+                random.Next(90, 5000).ToString(),
+                new TimePlaceRm("Praha Ruzyle", DateTime.Now.AddHours(random.Next(1, 55))),
+                new TimePlaceRm("Paris", DateTime.Now.AddHours(random.Next(4, 58))),
+                random.Next(1, 853)
+               ),
+           new (
+                Guid.NewGuid(), "AB Corporate Aviation",
+                random.Next(90, 5000).ToString(),
+                new TimePlaceRm("Le Bourget", DateTime.Now.AddHours(random.Next(1, 58))),
+                new TimePlaceRm("Zagreb", DateTime.Now.AddHours(random.Next(4, 60))),
+                random.Next(1, 853)
+               ),
+
         };
     
     }
